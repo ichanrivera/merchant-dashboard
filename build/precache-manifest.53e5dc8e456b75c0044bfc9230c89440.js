ef28d509a2b45266d48ba17194ebe733e134a592
ichanrivera <33001340+ichanrivera@users.noreply.github.com>
2019-09-20 17:29:31 +0800

    Add files via upload

---
+++ build/precache-manifest.53e5dc8e456b75c0044bfc9230c89440.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551b90e8b984cee5449939b744c167fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10089f2217cedb4d7671",
+    "url": "/static/css/main.acf53802.chunk.css"
+  },
+  {
+    "revision": "c47ce278008a3719e5ed",
+    "url": "/static/js/2.ffe46989.chunk.js"
+  },
+  {
+    "revision": "10089f2217cedb4d7671",
+    "url": "/static/js/main.29ee1673.chunk.js"
+  },
+  {
+    "revision": "3d39ccab5195c6735165",
+    "url": "/static/js/runtime~main.bcf556b0.js"
+  }
+]);
